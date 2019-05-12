de001daa34454dd5132f5b4b68678abfbd29ef31
John Taylor <john.taylor@doverfs.com>
2019-05-12 02:33:36 -0500

    Initial checkin

---
+++ src/Serilog.Enrichers.TraceIdentifier/TraceIdentifierLoggerConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Enrichers.TraceIdentifier.Enrichers;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Extends <see cref="LoggerConfiguration"/> to add enrichers for the current TraceIdentifier/>.
+    /// capabilities.
+    /// </summary>
+    public static class TraceIdentifierLoggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Enrich log events with a TraceIdentifier property containing the current <see cref="TraceIdentifier"/>.
+        /// </summary>
+        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration WithTraceIdentifier(
+           this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+            return enrichmentConfiguration.With<TraceIdentifierEnricher>();
+        }
+    }
+}
